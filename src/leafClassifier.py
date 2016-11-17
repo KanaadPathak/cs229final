1cb616ab997a629f3221c27c81f1636fd9cb5902
Albert Liu <peng_liu@apple.com>
2016-11-16 19:06:13 -0800

    fix bugs

--- src/leafClassifier.py
+++ src/leafClassifier.py
@@ -13,10 +13,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 
 #Train file format:
+#Kaggle
 #1st row -- comments
 #subsequent rows:
 #id,species(label),margin1,...,margin64,shape1,...,shape64,texture1,...,texture64
-
 class LeafDataSet():
   def __init__(self, csvfilename, scale=True):
     #read in features
@@ -26,7 +26,7 @@ class LeafDataSet():
     self.texture = self.data[:,128:192]
     #then read in labels sperately
     with open(csvfilename, 'r') as fd:
-    #skip the header
+      #skip the header
       fd.readline()
       self.labels={}; idx=0; y=[]
       for line in fd:
@@ -46,52 +46,66 @@ class LeafDataSet():
     with open(filename, 'w') as fd:
       json.dump(orderLabels,fd) 
 
-def trainLR(dataSet, reTrain=True):
-  bestC=1.0; minScore=float("inf")
-  if not reTrain:
-    bestC = 0.2
-  else:
-    for C in np.linspace(0.01,0.1,10):
-      clf=linear_model.LogisticRegression(penalty='l2', C=C, solver='sag', multi_class='multinomial', verbose=0, warm_start=False, n_jobs=-1)
-      scores = cross_val_score(clf, dataSet.X_train, dataSet.y_train, cv=10); m = scores.mean()
-      print("Training Error(C=%f): %0.2f (+/- %0.2f)" % (C, 1-m, scores.std() * 2))
-      if m<minScore: bestC=C; minScore=m;
+#Train file format:
+#UCI
+#Label, specimen number, eccentricity, ... (14 attributes)
+class LeafDataSetUCI():
+  def __init__(self, csvfilename, scale=True):
+    #read in features
+    self.data = np.loadtxt(csvfilename, delimiter=',', usecols=range(2,16))
+    #then read in labels sperately
+    self.target = np.loadtxt(csvfilename, delimiter=',', usecols=range(0,1), dtype=np.int)
+    self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.data, self.target, test_size=0.3)
+    if scale:
+      scaler = StandardScaler()
+      self.X_train = scaler.fit_transform(self.X_train)
+      self.X_test = scaler.transform(self.X_test)
+
+def trainLR(dataSet, CRange=np.logspace(-3,9,13), reTrain=True):
+  bestC=1.0; maxScore=0
+  for C in CRange:
+    clf=linear_model.LogisticRegression(penalty='l2', C=C, solver='sag', multi_class='multinomial', verbose=0, warm_start=False, n_jobs=-1)
+    scores = cross_val_score(clf, dataSet.X_train, dataSet.y_train, cv=10); m = scores.mean()
+    print("Training Error(C=%f): %0.2f (+/- %0.2f)" % (C, 1-m, scores.std() * 2))
+    if m>maxScore: bestC=C; maxScore=m;
   clf=linear_model.LogisticRegression(penalty='l2', C=bestC, solver='sag', multi_class='multinomial', verbose=0, warm_start=False, n_jobs=-1)
   clf.fit(dataSet.X_train, dataSet.y_train)
   return clf
 
-def trainSVMRBF(dataSet, reTrain=True):
-  if not reTrain:
-    #gamma=5 give 0.97. very sensitive to gamma, not C
-    bestC = 0.1; bestGamma = 5;
-  else:
-    C_2d_range = np.logspace(-1, 1, 3)
-    gamma_2d_range = np.linspace(1, 10, 20)
-    classifiers = []
-    minScore=float("inf");
-    #TODO: try different types of features
-    for C in C_2d_range:
-      for gamma in gamma_2d_range:
-        clf = SVC(C=C, gamma=gamma)
-        scores = cross_val_score(clf, dataSet.X_train, dataSet.y_train, cv=10); m = scores.mean()
-        print("Training Error(C=%f gamma=%f): %0.2f (+/- %0.2f)" % (C, gamma, 1-m, scores.std() * 2))
-        if m<minScore: minScore=m; (bestC, bestGamma) = (C, gamma)
+def trainSVMRBF(dataSet, CRange=np.logspace(-4,6,11), gammaRange=np.logspace(-5,9,15)):
+  classifiers = []
+  maxScore=0
+  #TODO: try different types of features
+  for C in CRange:
+    for gamma in gammaRange:
+      clf = SVC(C=C, gamma=gamma)
+      scores = cross_val_score(clf, dataSet.X_train, dataSet.y_train, cv=10); m = scores.mean()
+      print("Training Error(C=%f gamma=%f): %0.2f (+/- %0.2f)" % (C, gamma, 1-m, scores.std() * 2))
+      if m>maxScore: maxScore=m; (bestC, bestGamma) = (C, gamma)
   clf = SVC(C=bestC, gamma=bestGamma)
   clf.fit(dataSet.X_train, dataSet.y_train)
   return clf
 
-def load_kaggle():
-  return LeafDataSet(os.path.join(os.path.dirname(__file__), '../data/kaggle/train.csv'))
+def load_kaggle(scale=True):
+  return LeafDataSet(os.path.join(os.path.dirname(__file__), '../data/kaggle/train.csv'), scale)
+
+def load_uci(scale=True):
+  return LeafDataSetUCI(os.path.join(os.path.dirname(__file__), '../data/uci/leaf.csv'), scale)
+
+def selectModel(dataSet):
+  for model in (trainLR, trainSVMRBF):
+    clf = algo(dataSet)
+    predicts = clf.predict(dataSet.X_test)
+    print("%s Test Error: %0.2f" % (str(algo), np.sum(predicts!=dataSet.y_test)/float(np.size(dataSet.y_test))))
+
+def selectFeature(model, dataSet):
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description=__doc__, version=__version__)
   parser.add_argument('trainCsvFile', help = "supply train CSV file")
-  parser.add_argument('--dumpLabel', help = "dump labels to plist")
   args = parser.parse_args()
   dataSet = LeafDataSet(args.trainCsvFile)
+  dataSet = load_uci();
   if args.dumpLabel is not None:
     dataSet.writeLabelToJson(args.dumpLabel)
-  for algo in (trainLR, trainSVMRBF):
-    clf = algo(dataSet, reTrain=False)
-    predicts = clf.predict(dataSet.X_test)
-    print("%s Test Error: %0.2f" % (str(algo), np.sum(predicts!=dataSet.y_test)/np.size(dataSet.y_test.shape)))
+  selectModel(dataSet)
