c485cb5016f51e9266bf0f1b0af2bad1a8e17e24
Yangming Huang <leonmax@gmail.com>
2016-11-24 02:41:54 -0800

    auto load train and val with flavia data

--- src2/cnn.py
+++ src2/cnn.py
@@ -3,7 +3,8 @@ from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from argparse import ArgumentParser
-from yaml import load
+import yaml
+import os
 
 class CNNClassifier(object):
     def __init__(self, num_classes=2, img_height=150, img_width=150):
@@ -57,7 +58,46 @@ class CNNClassifier(object):
 
     def save(self, h5_file):
         self.model.save_weights(h5_file)
-        
+
+
+def get_classes(data_dir):
+    classes_ = []
+    for a_class in os.listdir(data_dir):
+        class_dir = '%s/%s' % (data_dir, a_class)
+        if os.path.isdir(class_dir):
+            classes_.append(a_class)
+    return classes_
+
+
+def count_image(data_dir):
+    supported_ext = ('.jpeg', '.jpg', '.png')
+    image_count = 0
+    for root, dirs, files in os.walk(data_dir):
+        image_count += sum(f.endswith(supported_ext) for f in files)
+    return image_count
+
+
+def load_generator(data_dir, target_size, batch_size, generator_params):
+    classes_ = get_classes(data_dir)
+    class_mode = 'binary' if len(classes) <= 2 else 'categorical'
+
+    data_gen = ImageDataGenerator(**generator_params)
+
+    return data_gen.flow_from_directory(
+        data_dir,
+        target_size=target_size,
+        classes=classes_,
+        class_mode=class_mode,
+        batch_size=batch_size)
+
+
+def make_divisible(total, divisor):
+    remainder = total % divisor
+    if remainder != 0:
+        return total + divisor - remainder
+    else:
+        return total
+
 
 if __name__ == '__main__':
     parser = ArgumentParser(description=__doc__)
@@ -67,30 +107,35 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     with open(args.config_file, 'r') as stream:
-        conf = load(stream)
+        conf = yaml.load(stream)
 
-    class_mode = 'binary'
-    if conf['num_classes'] > 2:
-        class_mode = 'categorical'
-    target_size = (conf['img_height'], conf['img_width'])
+    img_height = conf['img_height']
+    img_width = conf['img_width']
+    target_size = (img_height, img_width)
+    batch_size = conf['batch_size']
+    train_data_dir = '%s/train' % conf['data_dir']
+    val_data_dir = '%s/validation' % conf['data_dir']
 
-    train_generator = ImageDataGenerator(**conf['train_data']['generator']).flow_from_directory(
-        conf['train_data']['dir'],
-        target_size=target_size,
-        batch_size=conf['batch_size'],
-        class_mode=class_mode)
+    if 'num_train' in conf:
+        num_train = conf['num_train']
+    else:
+        num_train = make_divisible(count_image(train_data_dir), batch_size)
 
-    val_generator = ImageDataGenerator(**conf['val_data']['generator']
-                                       ).flow_from_directory(
-        conf['val_data']['dir'],
-        target_size=target_size,
-        batch_size=conf['batch_size'],
-        class_mode=class_mode)
+    if 'num_test' in conf:
+        num_test = conf['num_test']
+    else:
+        num_test = make_divisible(count_image(val_data_dir), batch_size)
 
-    clf = CNNClassifier(num_classes=conf['num_classes'], img_height=conf['img_height'], img_width=conf['img_width'])
-    clf.fit_generator(train_generator, val_generator,
-                      num_train=conf['train_data']['num'],
-                      num_val=conf['val_data']['num'],
+    train_gen_conf = conf['train_gen']
+    val_gen_conf = conf['val_gen']
+
+    classes = get_classes(train_data_dir)
+
+    clf = CNNClassifier(num_classes=len(classes), img_height=img_height, img_width=img_width)
+    clf.fit_generator(load_generator(train_data_dir, target_size, batch_size, train_gen_conf),
+                      load_generator(val_data_dir, target_size, batch_size, val_gen_conf),
+                      num_train=num_train,
+                      num_val=num_test,
                       num_epoch=args.epoch)
 
     if args.save_file is not None:
