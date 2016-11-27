8a0c3a2ca6fc5718589ff6a4feee20e1f5a09213
Yangming Huang <leonmax@gmail.com>
2016-11-27 01:56:44 -0800

    merge split_images preprocess_utils

--- src2/main.py
+++ src2/main.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 import argparse
-
 __version__ = "0.1"
 
 
 def main(args):
-    from cnn_feature import ClassifierPool, CNNFeatureExtractor
-    from cnn import run_cnn_classify
-
     if args.goal == 'extract':
+        from cnn_feature import CNNFeatureExtractor
         CNNFeatureExtractor().extract_feature(args.data_dir, args.feature_file)
+
     elif args.goal == 'classify':
+        from cnn_feature import ClassifierPool, CNNFeatureExtractor
         X, y = CNNFeatureExtractor().load_features(feature_file=args.feature_file)
         ClassifierPool().classify(X, y)
+
     elif args.goal == 'cnn_classify':
+        from cnn import run_cnn_classify
         run_cnn_classify(args)
+
+    elif args.goal == 'split':
+        from preprocess_utils import split_images
+        split_images(args.data_dir, args.train_size, args.test_size)
 
 
 if __name__ == '__main__':
@@ -26,8 +31,8 @@ if __name__ == '__main__':
     # ------------------------------------------------
     # extract
     extract_parser = subparsers.add_parser("extract")
-    extract_parser.add_argument('data_dir', help="the path to the config")
     extract_parser.add_argument('-f', '--feature_file', required=True, help="the file that the weight are saved to")
+    extract_parser.add_argument('data_dir', help="the path to the config")
     # ------------------------------------------------
     # classify
     classify_parser = subparsers.add_parser("classify")
@@ -37,6 +42,11 @@ if __name__ == '__main__':
     cnn_parser.add_argument('-e', '--epoch', type=int, default=50, help="the number of epochs to run")
     cnn_parser.add_argument('-s', '--save_file', help="the file that the weight are saved to")
     cnn_parser.add_argument('config_file', help="the path to the config")
+    # ------------------------------------------------
+    split_parser = subparsers.add_parser('split', description='split images in a folder to train and val')
+    split_parser.add_argument('--train_size', help="num of samples or proportion of samples for train")
+    split_parser.add_argument('--test_size', help="num of samples or proportion of samples for validation")
+    split_parser.add_argument('data_dir', help="the data dir with a images folder")
     # ================================================
 
     main(parser.parse_args())
--- src2/preprocess_utils.py
+++ src2/preprocess_utils.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 from keras.preprocessing.image import ImageDataGenerator
 import pandas as pd
@@ -38,3 +39,44 @@ def load_generator(data_dir, target_size=(256, 256), batch_size=32, **generator_
 
 def transform_values(dataframe, func):
     return pd.DataFrame(func(dataframe.values), columns=dataframe.columns, index=dataframe.index)
+
+
+def split_images(data_dir, test_size=None, train_size=None):
+    for a_class in os.listdir('%s/images/' % data_dir):
+        class_dir = '%s/images/%s' % (data_dir, a_class)
+        if not os.path.isdir(class_dir):
+            # non-dir is not a class folder
+            continue
+        print('splitting class: %s' % a_class)
+        train_class_dir = '%s/train/%s' % (data_dir, a_class)
+        val_class_dir = '%s/validation/%s' % (data_dir, a_class)
+        os.makedirs(train_class_dir, exist_ok=True)
+        os.makedirs(val_class_dir, exist_ok=True)
+
+        sample_list = os.listdir(class_dir)
+
+        train_bound = int(len(sample_list) * .75)
+        if train_size is not None:
+            if isinstance(train_size, float):
+                train_bound = int(len(sample_list) * train_size)
+            elif isinstance(train_size, int):
+                train_bound = train_size
+
+        test_bound = len(sample_list)
+        if train_size is not None:
+            if isinstance(test_size, float):
+                test_bound = train_bound + int(len(sample_list) * test_size)
+            elif isinstance(test_size, int):
+                test_bound = train_bound + test_size
+
+        count = 0
+        for f in sample_list:
+            supported_ext = ('.jpeg', '.jpg', '.png')
+            if f.lower().endswith(supported_ext):
+                if count < train_bound:
+                    shutil.copy('%s/%s' % (class_dir, f),
+                                '%s/%s' % (train_class_dir, f))
+                elif count < test_bound:
+                    shutil.copy('%s/%s' % (class_dir, f),
+                                '%s/%s' % (val_class_dir, f))
+                count += 1
