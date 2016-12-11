11dada62b5d5579e4c7f8e754d81b5dd4385ee7e
Albert Liu <peng_liu@apple.com>
2016-12-10 16:01:53 -0800

    merge cm from branch

--- src/confusion_matrix.py
+++ src/confusion_matrix.py
@@ -8,71 +8,76 @@ import logging
 from sklearn.metrics import confusion_matrix
 from sklearn.externals import joblib
 
-def plot_confusion_matrix(y_test, y_pred,
-                          classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    # Compute confusion matrix
-    #cm = confusion_matrix(y_test, y_pred)
-    cm = np.zeros((201,201))
 
-    for (i,j) in zip(y_test, y_pred):
-      cm[i,j] += 1
-    np.set_printoptions(precision=2)
-
-    #plt.figure()
-    #plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    #plt.title(title)
-    #plt.colorbar()
-    #tick_marks = np.arange(len(classes))
-    #plt.xticks(tick_marks, classes, rotation=45)
-    #plt.yticks(tick_marks, classes)
-
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    mismatch = []
-    thresh = cm.max() / 2.
+def print_cm(cm, classes, top):
+    mismatch = []; match = []
+    cnt = cm.astype('float').sum(axis=1)
+    cnt = np.where(cnt, cnt, np.ones(cnt.shape).astype('float'))
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        #plt.text(j, i, cm[i, j], horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
         if i != j and cm[i,j] != 0:
-          #print('%d->%d: %d' %( i, j, cm[i,j]))
-          mismatch.append([cm[i,j], i, j])
+            mismatch.append([cm[i,j], i, j, cm.astype('float')[i,j]/cnt[i]])
+        if i == j:
+            match.append([cm[i,j], i, cm.astype('float')[i,j]/cnt[i]])
+    match = sorted(match, reverse = True, key = lambda x: x[0])
     mismatch = sorted(mismatch, reverse = True, key = lambda x: x[0])
-    for i in range(50):
-      item = mismatch[i]
-      print('%s(%d)->%s(%d): %d' % (classes[item[1]], item[1], classes[item[2]], item[2], item[0]))
+    print("top %(top)d matched"%locals())
+    for i in range(top):
+        item = match[i]
+        print('%s(%d): %f(%d)' % (classes[item[1]], item[1], item[2],item[0]))
+    print("top %(top)d mismatch, :-("%locals())
+    for i in range(top):
+        item = mismatch[i]
+        print('%s(%d)->%s(%d): %f(%d)' % (classes[item[1]], item[1], classes[item[2]], item[2], item[3], item[0]))
 
+def plot_cm(y_test, y_pred, classes, visual=False, pretty_print=False, output=None, top=10,
+                     title='Confusion matrix', cmap=plt.cm.jet):
+    """ This function save the confusion matrix """
+    # Compute confusion matrix
+    cm = confusion_matrix(y_test, y_pred)
+    #cm = np.zeros((201,201))
 
-    #plt.tight_layout()
-    #plt.ylabel('True label')
-    #plt.xlabel('Predicted label')
-    #plt.show()
+    #for (i,j) in zip(y_test, y_pred):
+    #  cm[i,j] += 1
+    if pretty_print:
+        print_cm(cm, classes, top)
+
+    cnt = cm.sum(axis=1)
+    cnt =np.where( cnt, cnt, np.ones(cnt.shape))
+    #normalize
+    cm = cm.astype('float') / cnt[:, np.newaxis]
+
+    fig, ax = plt.subplots()
+    ax.set_title(title)
+    ax.set_ylabel('True label')
+    ax.set_xlabel('Predicted label')
+    cax = ax.matshow(cm, cmap=cmap)
+    fig.colorbar(cax)
+    if visual:
+        plt.show()
+    if output is not None:
+        plt.savefig(output)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description=__doc__)
-  parser.add_argument('-l', dest='logLevel', default='info',
-                      help="logging level: {debug, info, error}")
+  parser.add_argument('-l', dest='logLevel', default='info', help="logging level: {debug, info, error}")
+  parser.add_argument('--output', dest='output', help="save confusion matrix")
+  parser.add_argument('--visual', dest='visual', action='store_true', help="save confusion matrix")
+  parser.add_argument('--top', dest='top', type=int, default=10, help="save confusion matrix")
+  parser.add_argument('--pretty_print', dest='pretty_print', action='store_true', help="print confusion matrix")
   parser.add_argument('result_file', help="results")
   args = parser.parse_args()
 
-  logging.basicConfig(level=getattr(logging, args.logLevel.upper()),
-                      format='%(asctime)s %(levelname)s %(message)s')
+  logging.basicConfig(level=getattr(logging, args.logLevel.upper()), format='%(asctime)s %(levelname)s %(message)s')
 
   data = joblib.load(args.result_file)
   #d = {'y_predict': best_predict, 'y_test': y_test, 'score': best_score,'y_class': test_class}
   y_class = data['y_class']
   y_predict = data['y_predict']
   y_test = data['y_test']
-  score = data['score']
-  plot_confusion_matrix(y_test, y_predict, y_class)
-  logging.info("done")
+  plot_cm(y_test, y_predict, y_class,
+                   visual=args.visual,
+                   pretty_print=args.pretty_print,
+                   output=args.output,
+                   top=args.top,
+                   )
 
